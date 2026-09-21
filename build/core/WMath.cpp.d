1ec820c806b04fb95298ee3e812e8d454255c2b2
Ken Zaldo <ken@zaldo.com>
2026-09-21 18:12:45 -0400

    Remove Build Folder

--- build/core/WMath.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\ken\Documents\Arduino\MidiPhraseSampler\build\core\WMath.cpp.o: \
- C:\Users\ken\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.8\cores\arduino\WMath.cpp
--- build/libraries/Wire/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\ken\Documents\Arduino\MidiPhraseSampler\build\libraries\Wire\utility\twi.c.o: \
- C:\Users\ken\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.8\libraries\Wire\src\utility\twi.c \
- C:\Users\ken\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.8\cores\arduino/Arduino.h \
- C:\Users\ken\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.8\cores\arduino/binary.h \
- C:\Users\ken\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.8\variants\leonardo/pins_arduino.h \
- C:\Users\ken\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.8\libraries\Wire\src\utility\twi.h
